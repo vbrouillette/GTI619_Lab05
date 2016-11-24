698ba0e480cf205105ac62489680d1fa61b88510
vincent brouillette <vincentbrouillette100@hotmail.com>
2016-11-24 17:05:47 -0500

    password admin

--- GTI619_Lab5/Controllers/AdminController.cs
+++ GTI619_Lab5/Controllers/AdminController.cs
@@ -1,5 +1,7 @@
 ﻿using GTI619_Lab5.DAL;
 using GTI619_Lab5.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +14,12 @@ namespace GTI619_Lab5.Controllers
     {
 
         private ApplicationContext _context;
+        public UserManager<ApplicationUser> UserManager { get; private set; }
 
         public AdminController()
         {
             _context = new ApplicationContext();
+            UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
         }
 
         [Authorize(Roles = "Administrateur")]
@@ -42,34 +46,42 @@ namespace GTI619_Lab5.Controllers
                 NbAttemptsBeforeBlocking=loginConfig.NbAttemptsBeforeBlocking
             };
             return View(updateConfigModel);
-
         }
 
         [HttpPost]
         [Authorize(Roles = "Administrateur")]
         public ActionResult UpdateConfig(UpdateConfigModel model)
         {
-            var config = _context.AuthentificationConfigs.First();
-            config.TimeOutSession = model.TimeOutSession;
-            config.IsLowerCase = model.IsLowerCase;
-            config.IsNumber = model.IsNumber;
-            config.IsPeriodic = model.IsPeriodic;
-            config.IsSpecialCase = model.IsSpecialCase;
-            config.IsUpperCase = model.IsUpperCase;
-            config.MaxLenght = model.MaxLenght;
-            config.MinLenght = model.MinLenght;
-            config.PeriodPeriodic = model.PeriodPeriodic;
-            config.NbrLastPasswords = model.NbrLastPasswords;
+            var user = UserManager.Find(User.Identity.Name, model.Password);
+            if (user == null)
+            {
+                //TODO : Trouver quelque de meilleur
+                throw new Exception("Le mot de passe entré ne correspond pas à votre mot de passe de compte.");
+            }
+            else
+            {
+                var config = _context.AuthentificationConfigs.First();
+                config.TimeOutSession = model.TimeOutSession;
+                config.IsLowerCase = model.IsLowerCase;
+                config.IsNumber = model.IsNumber;
+                config.IsPeriodic = model.IsPeriodic;
+                config.IsSpecialCase = model.IsSpecialCase;
+                config.IsUpperCase = model.IsUpperCase;
+                config.MaxLenght = model.MaxLenght;
+                config.MinLenght = model.MinLenght;
+                config.PeriodPeriodic = model.PeriodPeriodic;
+                config.NbrLastPasswords = model.NbrLastPasswords;
 
-            var loginConfig = _context.LoginConfigs.First();
-            loginConfig.DelayBetweenBlocks = model.DelayBetweenBlocks;
-            loginConfig.DelayBetweenFailedAuthentication = model.DelayBetweenFailedAuthentication;
-            loginConfig.MaxBlocksBeforeAdmin = model.MaxBlocksBeforeAdmin;
-            loginConfig.NbAttemptsBeforeBlocking = model.NbAttemptsBeforeBlocking;
+                var loginConfig = _context.LoginConfigs.First();
+                loginConfig.DelayBetweenBlocks = model.DelayBetweenBlocks;
+                loginConfig.DelayBetweenFailedAuthentication = model.DelayBetweenFailedAuthentication;
+                loginConfig.MaxBlocksBeforeAdmin = model.MaxBlocksBeforeAdmin;
+                loginConfig.NbAttemptsBeforeBlocking = model.NbAttemptsBeforeBlocking;
 
-            _context.Entry(config).State = System.Data.Entity.EntityState.Modified;
-            _context.Entry(loginConfig).State = System.Data.Entity.EntityState.Modified;
-            _context.SaveChanges();         
+                _context.Entry(config).State = System.Data.Entity.EntityState.Modified;
+                _context.Entry(loginConfig).State = System.Data.Entity.EntityState.Modified;
+                _context.SaveChanges();
+            }
 
             return RedirectToAction("Manage");
         }
