2eef8ff32708538679b0a983bb8ec646690a28de
Simon Corcos <simoncorcos@yahoo.ca>
2016-11-28 16:03:23 -0500

    More db stuff

--- GTI619_Lab5/Scripts/SQL/2 - Roles.sql
+++ GTI619_Lab5/Scripts/SQL/2 - Roles.sql
@@ -1,3 +1,5 @@
-﻿INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'09b45d38-6c8f-4aaa-9745-790ed525c600', N'Cercle')
+﻿
+
+INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'09b45d38-6c8f-4aaa-9745-790ed525c600', N'Cercle')
 INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'68e5a8c3-a1b5-423b-9a2d-200cde18c6f8', N'Administrateur')
 INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'e136cf84-5180-447e-b9eb-53c2babeddb0', N'Carrée')
